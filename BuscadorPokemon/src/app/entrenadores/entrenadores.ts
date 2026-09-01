33304722a8be2a8000abee368e4eb27e10fe7536
SofiaRodriguez1302 <sofiguzmanrodriguez1302@gmail.com>
2026-09-01 17:40:13 +0000

    Listo

---
+++ BuscadorPokemon/src/app/entrenadores/entrenadores.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrenadores',
+  imports: [],
+  templateUrl: './entrenadores.html',
+  styleUrl: './entrenadores.css',
+})
+export class Entrenadores {}
